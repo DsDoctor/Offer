b2b8b74be0a2889bd9005d1f0178ca418f775e7c
dsdoctor <ds_doctor@foxmail.com>
2020-07-06 22:46:22 +0800

    《剑指offer》

---
+++ q9_stack_2_queue.py
@@ -0,0 +1,18 @@
+"""
+用两个栈实现一个队列。
+实现它的两个函数append_tail和delete_head，分别完成在队列尾部插入节点
+和在队列头部删除节点的功能
+"""
+import doctest
+
+
+def q9():
+    """
+    >>> q9()
+    :return:
+    """
+
+
+if __name__ == '__main__':
+    if not doctest.testmod().failed:
+        print('Well Done!')
